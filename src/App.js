f66ef6bc8ff1d603714b7df7c94b66080eae2a11
Lucca <lucca3447@hotmail.com>
2026-08-01 16:32:56 -0300

    chore: add .gitignore and .prettierrc, configure package.json for prettier

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
 const Pizza = (props) => {
-    return React.createElement(
-        "div",{}, [
-            React.createElement("h1",{}, props.name),
-            React.createElement("p", {}, props.description)
-        ]
-
-    )
-}
-
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
 
 const App = () => {
-    return React.createElement("div",{},[
-        React.createElement("h1",{}, "Padre Gino's"),
-        React.createElement(Pizza, {
-            name: "The portuguese pizza",
-            description: "Eggs, ham, muzzarela chesse, tomatoes"
-        }),
-        React.createElement(Pizza, {
-            name: "The bacon pizza",
-            description: "Bacon, corn and cheese"
-        }),
-        React.createElement(Pizza, {
-            name: "Strogonoff Pizza",
-            description: "Strogonoff"
-        }),
-        React.createElement(Pizza, {
-            name: "Pinneaple pizza",
-            description: "Some pineple pizza igredients that I can't make up"
-        }),
-        React.createElement(Pizza, {
-            name: "Peperoni Pizza",
-            description: "Idk what go in peperoni pizza, maybe peperoni and cheese idk"
-        }),
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The portuguese pizza",
+      description: "Eggs, ham, muzzarela chesse, tomatoes",
+    }),
+    React.createElement(Pizza, {
+      name: "The bacon pizza",
+      description: "Bacon, corn and cheese",
+    }),
+    React.createElement(Pizza, {
+      name: "Strogonoff Pizza",
+      description: "Strogonoff",
+    }),
+    React.createElement(Pizza, {
+      name: "Pinneaple pizza",
+      description: "Some pineple pizza igredients that I can't make up",
+    }),
+    React.createElement(Pizza, {
+      name: "Peperoni Pizza",
+      description:
+        "Idk what go in peperoni pizza, maybe peperoni and cheese idk",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container)
+const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
