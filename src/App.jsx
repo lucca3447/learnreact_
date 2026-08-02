0b22d475dd3a54820a5dc4b75f7d54d5dd14bb26
Lucca <lucca3447@hotmail.com>
2026-08-02 12:03:52 -0300

    feat: add image prop to Pizza component and update index.html for styling

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ const App = () => {
   return (
     <div>
       <h1>Lucca's - Order Now </h1>
-      <Pizza name="Pepperoni" description=" stuff of a peperoni pizza"/>
-      <Pizza name="Pineple" description=" stuff of a pinaple pizza"/>
-      <Pizza name="Portuguese" description=" stuff of a portuguese pizza"/>
+      <Pizza name="Pepperoni" description=" stuff of a peperoni pizza" image={"/public/pizzas/pepperoni.webp"}/>
+      <Pizza name="Pineple" description=" stuff of a pinaple pizza" image={"/public/pizzas/hawaiian.webp"}/>
+      <Pizza name="Portuguese" description=" stuff of a portuguese pizza" image={"/public/pizzas/big_meat.webp"} />
       
     </div>
   )
