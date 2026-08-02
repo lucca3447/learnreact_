51765ef85f16338ceb0b7fce0238f5b4d803fc7c
Lucca <lucca3447@hotmail.com>
2026-08-02 16:39:46 -0300

    feat: implement Order component for pizza selection and integrate into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza"
+import Order from './Order'
 
 
 
@@ -8,10 +8,7 @@ const App = () => {
   return (
     <div>
       <h1>Lucca's - Order Now </h1>
-      <Pizza name="Pepperoni" description=" stuff of a peperoni pizza" image={"/public/pizzas/pepperoni.webp"}/>
-      <Pizza name="Pineple" description=" stuff of a pinaple pizza" image={"/public/pizzas/hawaiian.webp"}/>
-      <Pizza name="Portuguese" description=" stuff of a portuguese pizza" image={"/public/pizzas/big_meat.webp"} />
-      
+      <Order />
     </div>
   )
 };
