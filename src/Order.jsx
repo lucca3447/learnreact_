f37c192588e7155c82086aaab8981228471ce7d6
Lucca <lucca3447@hotmail.com>
2026-08-02 22:04:33 -0300

    feat: enable dynamic pizza selection with state management in Order component

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react"
 import Pizza from "./Pizza"
 
 
 export default function Order (){
-    const pizzaType = "pepperoni"
-    const pizzaSize = "M"
+    //const pizzaType = "pepperoni"
+    //const pizzaSize = "M"
+    const [pizzaType, setPizzaType] = useState("pepperoni");
+    const [pizzaSize, setPizzaSize] = useState("M");
 
     return(
         <div className="order">
@@ -12,7 +15,14 @@ export default function Order (){
                 <div>
                     <div>
                         <label htmlFor="pizza-type">Pizza Type</label>
-                        <select name="pizza-type" value={pizzaType}>
+                        <select
+
+                        onChange={(e) => setPizzaType(e.target.value)}
+                        name="pizza-type" 
+                        value={pizzaType}
+                        >
+
+
                             <option value="pepperoni">The Pepperoni Pizza</option>
                             <option value="hawaiian">The Hawaiian Pizza</option>
                             <option value="big_meat">The Big Meat Pizza</option>
@@ -23,7 +33,7 @@ export default function Order (){
                         <label htmlFor="pizza-size">Pizza Size</label>
                         <div>
                             <span>
-                               <input 
+                               <input onChange={(e) => setPizzaSize(e.target.value)} 
                                checked={pizzaSize === 'S'}
                                type="radio"
                                name="pizza-size"
@@ -33,7 +43,7 @@ export default function Order (){
                                <label htmlFor="pizza-s">Small</label> 
                             </span>
                             <span>
-                               <input 
+                               <input onChange={(e) => setPizzaSize(e.target.value)} 
                                checked={pizzaSize === 'M'}
                                type="radio"
                                name="pizza-size"
@@ -43,7 +53,7 @@ export default function Order (){
                                <label htmlFor="pizza-m">Medium</label> 
                             </span>
                             <span>
-                               <input 
+                               <input onChange={(e) => setPizzaSize(e.target.value)} 
                                checked={pizzaSize === 'L'}
                                type="radio"
                                name="pizza-size"
