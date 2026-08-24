dfb2832caa0624c4a244938cd8da22530eef0cb2
Lucca <lucca3447@hotmail.com>
2026-08-23 22:08:18 -0300

    feat: add PizzaOfTheDay component and custom hook for fetching pizza data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from './Order'
 
@@ -6,10 +6,12 @@ import Order from './Order'
 
 const App = () => {
   return (
+    <StrictMode>
     <div>
       <h1>Lucca's - Order Now </h1>
       <Order />
     </div>
+    </StrictMode>
   )
 };
 
