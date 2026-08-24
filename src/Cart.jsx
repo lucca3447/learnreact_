afbcf3b322f7540c99d120c428faf7a3f6181f01
Lucca <lucca3447@hotmail.com>
2026-08-23 23:49:19 -0300

    feat: fix cart component logic and improve rendering of cart items

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -2,3 +2,28 @@ const intl = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "BRL",
 });
+
+export default function Cart({ cart, checkout }) {
+  let total = 0;
+  for (let i = 0; i < cart.lenght; i++) {
+    const current = cart[i];
+    total += current.pizza.sizes[current.size];
+  }
+
+  return (
+    <div className="cart">
+      <h2>Cart</h2>
+      <ul>
+        {cart.map((item, index) => {
+          <li key={index}>
+            <span className="size">{item.size}</span>
+            <span className="type">{item.name}</span>
+            <span className="price">{item.price}</span>
+          </li>;
+        })}
+      </ul>
+      <p>Total: {intl.format(total)}</p>
+      <button onClick={checkout}></button>
+    </div>
+  );
+}
