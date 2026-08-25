fdc12344eb314e42ba6f73c53ff12257a33772db
Lucca <lucca3447@hotmail.com>
2026-08-25 20:41:59 -0300

    feat: add Header component and integrate it into App feat: create CartContext for managing cart state

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import PizzaOfTheDay from "./PizzaOfTheDay";
 import Order from "./Order";
+import Header from "./Header";
 
 const App = () => {
   return (
     <StrictMode>
       <div>
-        <h1 className="logo">Lucca's - Order Now </h1>
+        <Header />
         <Order />
         <PizzaOfTheDay />
       </div>
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return (
+        <nav>
+            <h1 className="logo">Lucca's</h1>
+            <div className="nav-cart">
+                <span className="nav-cart-number">7</span>
+            </div>
+        </nav>
+    )
+}
