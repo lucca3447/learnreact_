38893509f4269a706232699336a627c0729eb806
Lucca <lucca3447@hotmail.com>
2026-08-28 13:15:05 -0300

    feat: integrate React Query by adding QueryClientProvider to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>);
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </StrictMode>
+  );
 };
 
 const container = document.getElementById("root");
