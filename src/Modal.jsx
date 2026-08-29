e0a98fb857b8954ee71dcf7a06b85aa87721bd04
Lucca <lucca3447@hotmail.com>
2026-08-29 16:27:23 -0300

    feat: add Modal component for rendering content in a portal

---
+++ src/Modal.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children })=>{
+    const elRef = useRef(null);
+    if(!elRef.current){
+        elRef.current = document.createElement("div")
+    }
+
+    useEffect(() => {
+        const modalRoot = document.getElementById("modal");
+        modalRoot.appendChild(elRef.current);
+        // return "TODO"
+    }, []);
+
+    return createPortal(<div>{children}</div>,elRef.current);
+
+}
+
+export default Modal;
+
+
+<div>
+    <h1>lol</h1>
+</div>
