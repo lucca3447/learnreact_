b249cb4d95f421b52713f614eaa2816c4b95b5f3
Lucca <lucca3447@hotmail.com>
2026-08-29 20:59:48 -0300

    test: add cleanup after each test in Pizza component tests

--- src/__tests__/Pizza.test.jsx
+++ src/__tests__/Pizza.test.jsx
@@ -1,6 +1,8 @@
-import { render } from "@testing-library/react";
-import { expect, test } from "vitest";
+import { render, cleanup } from "@testing-library/react";
+import { expect, test, afterEach } from "vitest";
 import Pizza from "../Pizza";
+
+afterEach(cleanup);
 
 test("alt test renders on Pizza image", async () => {
   const name = "I should be a pizza, but it's Kurisu";
@@ -12,4 +14,13 @@ test("alt test renders on Pizza image", async () => {
   const img = screen.getByRole("img");
   expect(img.src).toBe(src);
   expect(img.alt).toBe(name);
+});
+
+test("to have default image if none is provided", async () => {
+  const screen = render(
+    <Pizza name={"Cool Pizza"} description="super cool pizza" />
+  );
+
+  const img = screen.getByRole("img");
+  expect(img.src).not.toBe("");
 });
