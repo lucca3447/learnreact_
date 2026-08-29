9e963795deb202eca7543cc7abd92d3283ddadd4
Lucca <lucca3447@hotmail.com>
2026-08-29 20:39:40 -0300

    test: add unit test for Pizza component image rendering

---
+++ src/__tests__/Pizza.test.jsx
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import { expect, test } from "vitest";
+import Pizza from "../Pizza";
+
+test("alt test renders on Pizza image", async () => {
+  const name = "My favorite pizza";
+  const src =
+    "https://i.pinimg.com/1200x/97/a8/14/97a81445e810015073e9ad30fa903042.jpg";
+  const screen = render(
+    <Pizza name={name} description="Cool Kurisu" image={src} />,
+  );
+  const img = screen.getByRole("img");
+  expect(img.src).toBe(src);
+  expect(img.alt).toBe(name);
+});
