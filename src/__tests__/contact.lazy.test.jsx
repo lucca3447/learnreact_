6ceccf7e4f1fd49a0874ee6d55452cc6437f86af
Lucca <lucca3447@hotmail.com>
2026-09-01 13:08:31 -0300

    chore: update package.json and package-lock.json to include vitest-fetch-mock and cross-fetch test: add initial test setup for contact.lazy component

---
+++ src/__tests__/contact.lazy.test.jsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import { expect, test, vi } from "vitest";
+import createFetchMock from "vitest-fetch-mock";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { Route } from "../routes/contact.lazy";
+
+const queryClient = new QueryClient();
+
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks();
