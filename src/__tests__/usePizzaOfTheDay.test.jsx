2ab63874c10a46ad69516dc8591d84ceb954f56c
Lucca <lucca3447@hotmail.com>
2026-09-01 21:58:21 -0300

    test: refactor usePizzaOfTheDay test to use renderHook for initial state verification

--- src/__tests__/usePizzaOfTheDay.test.jsx
+++ src/__tests__/usePizzaOfTheDay.test.jsx
@@ -1,5 +1,5 @@
 import { expect, test, vi } from "vitest";
-import { render } from "@testing-library/react";
+import { render, renderHook } from "@testing-library/react";
 import createFetchMock from "vitest-fetch-mock";
 import { usePizzaOfTheDay } from "../usePizzaOfTheDay";
 
@@ -31,6 +31,6 @@ function getPizzaOfTheDay() {
 
 test("gives null when first called", async () => {
   fetch.mockResponseOnce(JSON.stringify(testPizza));
-  const pizza = getPizzaOfTheDay();
-  expect(pizza).toBeNull();
+  const {result} = renderHook(() => usePizzaOfTheDay())
+  expect(result.current).toBeNull();
 });
