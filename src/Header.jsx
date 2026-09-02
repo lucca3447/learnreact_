8199af56bd06c8b9b02801e2eab850a2608f2ffc
Lucca <lucca3447@hotmail.com>
2026-09-01 23:45:36 -0300

    refactor: standardize formatting and improve readability in Header component

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,17 @@
-import { useContext } from "react"
-import { CartContext } from "./contexts"
-import { Link } from "@tanstack/react-router"
+import { use } from "react";
+import { CartContext } from "./contexts";
+import { Link } from "@tanstack/react-router";
 
-export default function Header(){
-    const [cart] = useContext(CartContext)
-    return (
-        <nav>
-            <Link to="/">
-            <h1 className="logo">Lucca's</h1>
-            </Link>
-            <div className="nav-cart">
-                <span className="nav-cart-number">{cart.length}</span>
-            </div>
-        </nav>
-    )
+export default function Header() {
+  const [cart] = use(CartContext);
+  return (
+    <nav>
+      <Link to="/">
+        <h1 className="logo">Lucca's</h1>
+      </Link>
+      <div className="nav-cart">
+        <span className="nav-cart-number">{cart.length}</span>
+      </div>
+    </nav>
+  );
 }
