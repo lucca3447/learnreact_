2ede172d32b632b0c5d4580ecab6acc062a7f0c2
Lucca <lucca3447@hotmail.com>
2026-09-01 23:27:39 -0300

    refactor: replace input elements with ContactInput component for better form handling

--- src/routes/contact.lazy.jsx
+++ src/routes/contact.lazy.jsx
@@ -1,3 +1,4 @@
+import { useFormStatus } from "react-dom";
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { useMutation } from "@tanstack/react-query";
 import postContact from "../api/postContact";
@@ -24,12 +25,24 @@ function ContactRoute() {
         <h3>Submitted!</h3>
       ) : (
         <form action={mutation.mutate}>
-          <input name="name" placeholder="Name" />
-          <input type="email" name="email" placeholder="Email" />
-          <textarea placeholder="Message" name="message"></textarea>
+          <ContactInput name="name" placeholder="Name" />
+          <ContactInput type="email" name="email" placeholder="Email" />
+          <ContactInput placeholder="Message" name="message"></textarea>
           <button>Submit</button>
         </form>
       )}
     </div>
+  );
+}
+
+function ContactInput() {
+  const {pending} = useFormStatus();
+  return (
+    <input
+      disabled={pending}
+      name={Props.name}
+      type={props.type}
+      placeholder={props.placeholder}
+    ></input>
   );
 }
